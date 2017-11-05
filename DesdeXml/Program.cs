c33c818faa343395c96709a6d84f08d71ee7c290
mahpa94 <mahpatlv94@gmail.com>
2017-11-05 13:33:22 -0500

    UsoLinQ
    Signed-off-by: mahpa94 <mahpatlv94@gmail.com>

---
+++ DesdeXml/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DesdeXml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XDocument document = XDocument.Load("../../libros.xml");
+            Program.PrecioPromedio(document);
+            Console.Read();
+        }
+
+        private static void PrecioPromedio(XDocument document)
+        {
+            var AutorPrecio = from element in document.Descendants("catalog").Elements("book")
+                              select new
+                              {
+                                  autor = element.Element("author").Value,
+                                  precio = double.Parse(element.Element("price").Value)
+                              };
+
+            var AutorPrecioPromedio = from autorPrecio in AutorPrecio
+                                      group autorPrecio by new { autorPrecio.autor } into agrupamiento
+                                      select new
+                                      {
+                                          autor = agrupamiento.Key.autor,
+                                          precioPorLibro = agrupamiento.Average(x => x.precio)
+                                      };
+            var ListaDeAutorPrecioPromedio = AutorPrecioPromedio.ToList();
+            ListaDeAutorPrecioPromedio.ForEach(x => Console.WriteLine(x.autor + " - " + x.precioPorLibro));
+        }
+    }
+}
---
+++ UsoDeLinQ/Program.cs
@@ -0,0 +1,33 @@
+﻿using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsoDeLinQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program.ListarUsuarioConVentas();
+            Console.Read();
+        }
+
+        private static void ListarUsuarioConVentas()
+        {
+            using (CMV_LOCAL3Entities db = new CMV_LOCAL3Entities())
+            {
+                var query = from ventas in db.Venta_
+                            join grupo in db.Usuario_ on ventas.idUsuario equals grupo.username
+                            select new
+                            {
+                                TotalParaPagar = ventas.totalAPagar,
+                                NombreUsuario = grupo.nombre
+                            };
+                query.ToList().ForEach(x => Console.WriteLine(x.NombreUsuario + " - " + x.TotalParaPagar));
+            }
+        }
+    }
+}
